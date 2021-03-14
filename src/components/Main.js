adf80f95a3c2efaa914fe28d583e2068f341455e
Alexey Peresypkin <ugo200@yandex.ru>
2021-03-14 13:25:52 +0500

    Compose: renderCard

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ function Main(props) {
   let [userName, setUserName] = React.useState();
   let [userDescription, setUserDescription] = React.useState();
   let [userAvatar, setUserAvatar] = React.useState();
+  let [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
     api.getUserData()
@@ -15,7 +16,35 @@ function Main(props) {
       userAvatar = setUserAvatar(res.avatar);
     }) 
     .catch(err => console.log(err));
-  }, [userName, userDescription, userAvatar]);
+
+    api.getAllCards() 
+    .then(res => {
+      const cardList = res.map(item => {
+        return (
+            <li className="element">
+              <div className="element__image-box">
+                <img src={`${item.link}`} alt="Фото красивого места" className="element__image" />
+                <button type="button" className="element__remove-button">
+                  <img src="<%=require('./images/element-removeButton.svg')%>" alt="Удалить" />
+                </button>
+              </div>
+              <div className="element__text-box">
+                <h2 className="element__title">{item.name}</h2>
+                <div className="element__like-box">
+                  <button type="button" className="element__button">
+                    <img src="<%=require('./images/element-like.svg')%>" alt="Нравится" />
+                  </button>
+                  <p className="element__likes-number">{item.likes.length}</p>
+                </div>
+              </div>
+            </li>
+        )
+      });
+
+    cards = setCards(...[cardList]);
+  }) 
+  .catch(err => console.log(err));
+  }, [userName, userDescription, userAvatar, cards]);
 
   return (
     <>
@@ -35,27 +64,7 @@ function Main(props) {
         <button type="button" onClick={props.onAddPlace} className="profile__add-button" />
       </section>
       <section className="cards-container">
-        <ul className="elements">
-          <template className="card">
-            <li className="element">
-              <div className="element__image-box">
-                <img src="#" alt="Фото красивого места" className="element__image" />
-                <button type="button" className="element__remove-button">
-                  <img src="<%=require('./images/element-removeButton.svg')%>" alt="Удалить" />
-                </button>
-              </div>
-              <div className="element__text-box">
-                <h2 className="element__title"></h2>
-                <div className="element__like-box">
-                  <button type="button" className="element__button">
-                    <img src="<%=require('./images/element-like.svg')%>" alt="Нравится" />
-                  </button>
-                  <p className="element__likes-number" />
-                </div>
-              </div>
-            </li>
-          </template>  
-        </ul>
+        <ul className="elements">{cards}</ul>
       </section>
   </>
   );
