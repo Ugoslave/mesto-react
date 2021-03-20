121007667558002eca056281a79b5f5d20382f4b
Alexey Peresypkin <ugo200@yandex.ru>
2021-03-20 17:50:28 +0500

    Compose: Card by context

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,19 @@
 import removeButton from "../images/element-removeButton.svg";
 import likeButton from "../images/element-like.svg";
+import React from "react";
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
 function Card ({ card, onCardClick }) {
+  const user = React.useContext(CurrentUserContext);
+  const isOwn = card.owner._id === user.id;
+  const cardDeleteButtonClassName = (
+    `element__remove-button ${isOwn ? 'element__remove-button' : 'element__remove-button_disactive'}`
+  ); 
+  const isLiked = card.likes.some(i => i._id === user.id);
+  const cardLikeButtonClassName = (
+    `element__button ${isLiked ? 'element__button_active' : 'element__button'}`
+  );
+
   function handleClick() {
     onCardClick(card);
   }
@@ -15,14 +27,14 @@ function Card ({ card, onCardClick }) {
           className="element__image"
           onClick={handleClick}
         />
-        <button type="button" className="element__remove-button">
+        <button type="button" className= {cardDeleteButtonClassName}>
           <img src={removeButton} alt="Удалить" />
         </button>
       </div>
       <div className="element__text-box">
         <h2 className="element__title">{card.name}</h2>
         <div className="element__like-box">
-          <button type="button" className="element__button">
+          <button type="button" className= {cardLikeButtonClassName}>
             <img src={likeButton} alt="Нравится" />
           </button>
           <p className="element__likes-number">{card.likes.length}</p>
